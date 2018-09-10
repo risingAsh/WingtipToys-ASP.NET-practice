986f61bcaf603ec62535ba4ab2a9d161e5b02820
Kenneth Martin <ktm064@gmail.com>
2018-09-10 16:04:56 -0700

    Added data model and seed data

---
+++ WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity; //used to access functionality of Entity Framework (query, insert, update, delete)
+namespace WingtipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
